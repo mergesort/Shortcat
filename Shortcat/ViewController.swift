fa70933f5de100cafe255abfcab50f82e2ca289a
Joe Fabisevich <jfabisevich@twitter.com>
2019-01-08 15:22:21 -0500

    Initial Commit

---
+++ Shortcat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shortcat
+//
+//  Created by Joe Fabisevich on 1/8/19.
+//  Copyright © 2019 Joe Fabisevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
